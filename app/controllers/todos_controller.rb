bce26dc44ad5449536fe4d581775a0c7618f357b
priyadharshinivijay <vpriyaa25@gmail.com>
2021-03-04 17:30:47 +0530

    Dropped db and created

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,7 @@
 class TodosController < ApplicationController
     def index
-        render plain:Todo.order(:due_date).map {|todo| todo.to_displayable_string}.join("\n")
-        #render "index"
+        #render plain:Todo.order(:due_date).map {|todo| todo.to_displayable_string}.join("\n")
+        render "index"
 
     end
     def show
