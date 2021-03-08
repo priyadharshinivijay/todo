2b99e9b1893afe11c136cb309148d769135eafb8
priyadharshinivijay <vpriyaa25@gmail.com>
2021-03-08 13:29:00 +0530

    changes done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
     skip_before_action :ensure_user_logged_in
     def new
+        render "new"
     end
     def create
         user=User.find_by(email: params[:email])
